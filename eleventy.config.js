af0be4f045c28a458492d30c47f6e391e5628a4f
beauferris <h-beauferris@hotmail.com>
2025-11-11 00:30:08 -0700

    Fixed weird hdr images

--- eleventy.config.js
+++ eleventy.config.js
@@ -9,6 +9,38 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("split", function (str, delimiter) {
     return str.split(delimiter);
   });
+
+  const serviceOrder = [
+    "Residential Standard and Standing Seam Metal Roof Replacements",
+    "Residential Leak Repairs and Maintenance",
+    "Flat Roofing Commercial and Residential Replacements",
+    "Flat Roofing Leak Repairs and Maintenance",
+    "Insulation Top Ups",
+    "Special Projects",
+  ];
+  const serviceOrderMap = serviceOrder.reduce((map, title, index) => {
+    map[title] = index;
+    return map;
+  }, {});
+
+  eleventyConfig.addCollection("servicesOrdered", (collectionApi) => {
+    return collectionApi
+      .getFilteredByTag("posts")
+      .sort((a, b) => {
+        const aIndex =
+          serviceOrderMap.hasOwnProperty(a.data.title)
+            ? serviceOrderMap[a.data.title]
+            : serviceOrder.length;
+        const bIndex =
+          serviceOrderMap.hasOwnProperty(b.data.title)
+            ? serviceOrderMap[b.data.title]
+            : serviceOrder.length;
+        if (aIndex === bIndex) {
+          return (a.data.title || "").localeCompare(b.data.title || "");
+        }
+        return aIndex - bIndex;
+      });
+  });
   
   // Ensure directories exist before writing for permalink-based routes
   eleventyConfig.on("beforeBuild", () => {
